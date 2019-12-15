ff772046507374156944d650af840fee9a5f9f72
User <s@s.net>
2019-12-15 15:44:54 +0100

    1

---
+++ app/src/main/java/com/example/kvadratpifagora/ActivitySearch.kt
@@ -0,0 +1,29 @@
+package com.example.kvadratpifagora
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ActivitySearch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+
+        fun onClick(view: View) {
+            val b = view as Button
+            when (b.id) {
+                R.id.btnSearchBack -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+            }
+        }
+
+    }
+}
